de9f5f9a7e3b80569558430fa5c4d9765ef04a8e
AStengel54 <amstengel@quinnipiac.edu>
2015-10-15 17:14:28 +0000

    Add user delete view

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns= patterns ('',
@@ -14,7 +14,7 @@ urlpatterns= patterns ('',
    url(r'^question/(?P<pk>\d+)/answer/create/$', login_required(AnswerCreateView.as_view()), name='answer_create'),
    url(r'^question/(?P<question_pk>\d+)/answer/update/(?P<answer_pk>\d+)/$', login_required(AnswerUpdateView.as_view()), name='answer_update'),
    url(r'^question/(?P<question_pk>\d+)/answer/delete/(?P<answer_pk>\d+)/$', login_required(AnswerDeleteView.as_view()), name='answer_delete'),
-   url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),  
-   url(r'^user/(?P<slug>\w+)/$', login_required(UserDetailView.as_view()), name='user_detail'),  
-   url(r'^user/update/(?P<slug>\w+)/$', login_required(UserUpdateView.as_view()), name='user_update'),                    
+   url(r'^vote/$', login_required(VoteFormView.as_view()), name='vote'),
+   url(r'^user/(?P<slug>\w+)/$', login_required(UserDetailView.as_view()), name='user_detail'),
+   url(r'^user/update/(?P<slug>\w+)/$', login_required(UserUpdateView.as_view()), name='user_update'),              url(r'^user/delete/(?P<slug>\w+)/$', login_required(UserDeleteView.as_view()), name='user_delete'),
 )
